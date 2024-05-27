90c924c58c006e32b0b9e213725198200f1bf021
DowcoMaster <quentin.makinson@hotmail.com>
2024-05-26 21:08:41 -0700

    Added the boneyard class and methods

---
+++ BoneyardClasses/Boneyard.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoneyardClasses
+{
+    public class Boneyard
+    {
+        private List<Domino> listOfDominos = new List<Domino>();
+        public int DominosRemaining
+        {
+            get
+            {
+                return listOfDominos.Count;
+            }
+        }
+        public Domino this[int index]
+        {
+            get
+            {
+                return listOfDominos[index];
+            }
+            set
+            {
+                listOfDominos[index] = value;
+            }
+        }
+        public void BoneYard(int maxDots)
+        {
+            listOfDominos.Clear();
+            for (int i = 0; i <= maxDots; i++)
+            {
+                for (int j = i; j <= maxDots; j++)
+                {
+                    Domino domino = new Domino(i, j);
+                    listOfDominos.Add(domino);
+                }
+            }
+        }
+        public bool IsEmpty()
+        {
+            if (listOfDominos.Count == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public Domino Draw()
+        {
+            if (listOfDominos.Count == 0)
+            {
+                return null;
+            }
+            else
+            {
+                Domino domino = listOfDominos[0];
+                listOfDominos.RemoveAt(0);
+                return domino;
+            }
+        }
+        public void Shuffle()
+        {
+            Random gen = new Random();
+            for (int i = 0; i < listOfDominos.Count; i++)
+            {
+                int rnd = gen.Next(0, listOfDominos.Count);
+                Domino domino = listOfDominos[rnd];
+                listOfDominos[rnd] = listOfDominos[i];
+                listOfDominos[i] = domino;
+            }
+        }
+        public override string ToString()
+        {
+            string output = "";
+            foreach (Domino domino in listOfDominos)
+            {
+                output += domino.ToString() + "\n";
+            }
+            return output;
+        }
+    }
+    public class Domino
+    {
+        public int LeftDot { get; set; }
+        public int RightDot { get; set; }
+        public Domino(int leftDot, int rightDot)
+        {
+            LeftDot = leftDot;
+            RightDot = rightDot;
+        }
+        public override bool Equals(object obj)
+        {
+            return base.Equals(obj);
+        }
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+        public override string ToString()
+        {
+            return (LeftDot + RightDot).ToString();
+        }
+    }
+}
---
+++ BoneyardClasses/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using BoneyardClasses;
+
+class Program
+{
+    public static Boneyard boneyard = new Boneyard();
+    public static void Main(string[] args)
+    {
+        Start();
+    }
+
+    public static void Start()
+    {
+        Console.WriteLine("What would you like to do?");
+        Console.WriteLine("1 = Make a new boneyard of Dominos");
+        Console.WriteLine("2 = Draw a Domino from the boneyard");
+        Console.WriteLine("3 = Shuffle the boneyard");
+        Console.WriteLine("4 = Check if the boneyard is empty");
+        Console.WriteLine("5 = Check the number of Dominos remaining in the boneyard");
+        Console.WriteLine("6 = Print the boneyard");
+        int choice = Convert.ToInt32(Console.ReadLine());
+        switch (choice)
+        {
+            case 1:
+                Console.WriteLine("How many dots would you like on each side?");
+                int maxDots = Convert.ToInt32(Console.ReadLine());
+                boneyard.BoneYard(maxDots);
+                Console.WriteLine("New boneyard created with " + maxDots + " max dots on each side.");
+                break;
+            case 2:
+                Domino drawnDomino = boneyard.Draw();
+                if (drawnDomino != null)
+                {
+                    Console.WriteLine("You drew a " + drawnDomino.ToString());
+                }
+                else
+                {
+                    Console.WriteLine("The boneyard is empty and nothing to draw.");
+                }
+                break;
+            case 3:
+                boneyard.Shuffle();
+                Console.WriteLine("Boneyard shuffled.");
+                break;
+            case 4:
+                if (boneyard.IsEmpty())
+                {
+                    Console.WriteLine("The boneyard is empty.");
+                }
+                else
+                {
+                    Console.WriteLine("The boneyard is not empty.");
+                }
+                break;
+            case 5:
+                Console.WriteLine("There are " + boneyard.DominosRemaining + " Dominos remaining in the boneyard.");
+                break;
+            case 6:
+                Console.WriteLine(boneyard.ToString());
+                break;
+            default:
+                Console.WriteLine("Not a valid choice. Please try again.");
+                break;
+        }
+        Start();
+    }
+}
